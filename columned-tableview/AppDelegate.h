ce76c6376b32dfbd081703c3bd651b2d1a24c2c7
Cameron Cooke <cameron@brightec.co.uk>
2012-02-27 14:41:19 +0000

    Initial Commit

---
+++ columned-tableview/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  columned-tableview
+//
+//  Created by Cameron Cooke on 27/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ columned-tableview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  columned-tableview
+//
+//  Created by Cameron Cooke on 27/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
